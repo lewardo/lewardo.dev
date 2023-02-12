aa4cd5d7218fbdf4f055dc4a88268034d6b279c9
lewardo <lewardo23@gmail.com>
2023-02-12 18:15:07 +0000

    commented deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,22 +12,21 @@ cd dist
 # place .nojekyll to bypass Jekyll processing
 echo > .nojekyll
 
-# copy LICENSE and README from directory
+# copy LICENSE and README from parent directory
 cp ../LICENSE .
 cp ../README_deploy.md ./README.md
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# for when i own the domain name
+# echo 'lewardo.dev' > CNAME
 
+# initialise git if not present, checkout to main branch, add all changes and commit with timestamp
 git init
 git checkout -B main
 git add -A
 git commit -m "deploy $(date +%d%m%y_%H%M%S)"
 
-# if you are deploying to https://<USERNAME>.github.io
+# push to lewardo.github.io repo
 git push -f https://github.com/lewardo/lewardo.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
+# return to parent directory
 cd -
